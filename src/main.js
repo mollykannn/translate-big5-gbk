2efdbea13ee7fd1daf5e47d39e56174ba51736c4
Molly Kan <mollykannn@gmail.com>
2022-08-10 20:38:55 +0800

    Web share api function

--- src/main.js
+++ src/main.js
@@ -1,13 +1,6 @@
 import '/src/css/style.scss'
 import s2t from "/src/js/s2t-char.min.json"
 import t2s from "/src/js/t2s-char.min.json"
-import { registerSW } from 'virtual:pwa-register'
-
-const updateSW = registerSW({
-  onNeedRefresh () {},
-  onOfflineReady () {}
-})
-updateSW()
 
 const mapping = {
   s2t: new Map(Object.entries(s2t)),
@@ -18,3 +11,9 @@ window.convert = function (action) {
   let curDoc = document.getElementById("textOutput");
   curDoc.value =  [...curDoc.value].map((element) => mapping[action].get(element) || element).join('');
 };
+
+const url = new URL(window.location.href)
+let params = new URLSearchParams(url.search);
+if (params.has('text')) {
+  document.getElementById("textOutput").value = params.get('text')
+}
--- vite.config.js
+++ vite.config.js
@@ -1,53 +1,60 @@
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import StylelintPlugin from 'vite-plugin-stylelint';
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import StylelintPlugin from "vite-plugin-stylelint";
 
 export default defineConfig({
-  base: '/translate-big5-gbk',
+  base: "/translate-big5-gbk",
   plugins: [
     StylelintPlugin(),
     VitePWA({
-      mode: 'development',
-      srcDir: 'src',
-      filename: 'sw.js',
-      base: '/translate-big5-gbk/',
-      registerType: 'autoUpdate',
-      strategies: 'injectManifest',
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      workbox: {
+        sourcemap: true,
+      },
+      mode: "development",
+      base: "/translate-big5-gbk/",
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
       manifest: {
-        name: '繁簡轉換',
-        short_name: '繁簡轉換',
-        description: '繁簡轉換',
-        theme_color: '#0079d2',
-        start_url: './index.html',
-        lang: 'zh-Hant-HK',
-        dir: 'ltr',
-        orientation: 'portrait',
+        name: "繁簡轉換",
+        short_name: "繁簡轉換",
+        description: "繁簡轉換",
+        theme_color: "#0079d2",
+        start_url: "./index.html",
+        lang: "zh-Hant-HK",
+        dir: "ltr",
+        orientation: "portrait",
+        share_target: {
+          action: "/translate-big5-gbk/",
+          method: "GET",
+          params: {
+            text: "text"
+          },
+        },
         icons: [
           {
-            src: 'img/icons/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "img/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'img/icons/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: "img/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
           {
-            src: 'img/icons/android-chrome-maskable-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable'
+            src: "img/icons/android-chrome-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
           },
           {
-            src: 'img/icons/android-chrome-maskable-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-          }
-        ]
-      }
-    })
-  ]
-})
+            src: "img/icons/android-chrome-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
