6a4bce37519f7961ef986c6645f3d5f5ed7377ea
mollykannn <mollykannn@gmail.com>
2023-02-01 01:06:59 +0000

    Deploying to gh-pages from  @ 74aabfb14dd604091c5dcf01198ab0be4f390757 🚀

--- sw.js
+++ sw.js
@@ -90,14 +90,14 @@ define(['./workbox-3589c0c5'], (function (workbox) { 'use strict';
    */
 
   workbox.precacheAndRoute([{
-    "url": "assets/index.034962e2.js",
+    "url": "assets/index-30bdc664.css",
     "revision": null
   }, {
-    "url": "assets/index.30bdc664.css",
+    "url": "assets/index-650ebdf3.js",
     "revision": null
   }, {
     "url": "index.html",
-    "revision": "cec53507e0a7e80b3d6f140aba1319fa"
+    "revision": "ee893d0b2468f2e99b8711bb84ee6957"
   }, {
     "url": "registerSW.js",
     "revision": "d692813a4e6d08a0e5b6394c4f729574"
